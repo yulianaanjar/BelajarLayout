88b0f9976617d841d7c75aa5d297952d5b6ff121
yulianaanjar <48912565+yulianaanjar@users.noreply.github.com>
2019-04-02 00:18:14 +0700

    Initial commit

---
+++ app/src/main/java/anjaryuliana/ya/gmail/com/MainActivity.java
@@ -0,0 +1,13 @@
+package anjaryuliana.ya.gmail.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
